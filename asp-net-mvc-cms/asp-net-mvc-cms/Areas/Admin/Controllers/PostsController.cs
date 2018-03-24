af088a6ac2583b634c19cb20742738ccb3c017d1
ROOT <ROOT@ROOT>
2018-03-24 15:37:09 +0800

    2. Add Admin Area, Post Model, and Add PostsController

---
+++ asp-net-mvc-cms/asp-net-mvc-cms/Areas/Admin/Controllers/PostsController.cs
@@ -0,0 +1,63 @@
+﻿using asp_net_mvc_cms.Models;
+using System.Web.Mvc;
+
+namespace asp_net_mvc_cms.Areas.Admin.Controllers
+{
+    // /admin/posts
+    [RouteArea("admin")]
+    [RoutePrefix("post")]
+    public class PostsController : Controller
+    {
+        // GET: admin/posts
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        // /admin/posts/create
+        [HttpGet]
+        [Route("create")]
+        public ActionResult Create()
+        {
+            var post = new Post();
+
+            return View(post);
+        }
+
+        // /admin/posts/create
+        [HttpPost]
+        [Route("create")]
+        public ActionResult Create(Post post)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(post);
+            }
+
+            return RedirectToAction("index");
+        }
+
+        // /admin/post/edit/post-to-edit
+        [HttpGet]
+        [Route("edit/{id}")]
+        public ActionResult Edit(string id)
+        {
+            var post = new Post();
+
+            return View(post);
+        }
+
+        // /admin/post/edit/post-to-edit
+        [HttpPost]
+        [Route("edit/{id}")]
+        public ActionResult Edit(Post post)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(post);
+            }
+
+            return RedirectToAction("index");
+        }
+    }
+}
---
+++ asp-net-mvc-cms/asp-net-mvc-cms/Models/Post.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace asp_net_mvc_cms.Models
+{
+    public class Post
+    {
+        public string Id { get; set; }
+
+        public string  Title { get; set; }
+
+        public string Content { get; set; }
+
+        public DateTime Created { get; set; }
+
+        public DateTime? Published { get; set; }
+
+        public int AuthorId { get; set; }
+    }
+}
